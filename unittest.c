b108b9eacdaf4b1647beb1d6e3209fa97fa55e93
Angus Samuel Hilts <ashilts@uwaterloo.ca>
2017-07-24 16:00:43 -0400

    Basic framework is done.

--- unittest.c
+++ unittest.c
@@ -7,19 +7,46 @@
 // Simple unit test framework for C
 
 #include <stdio.h>
+#include "unittest.h"
 
-int tests_run;
+int tests_run = 0;
+int tests_passed = 0;
+int tests_failed = 0;
 
-
-int square(x) {
+int square(int x) {
 	return x*x;
 }
 
 // WRITE TESTS HERE
 
+int test01() {
+	ut_assert( square(5) == 25 );
+	return 0;
+}
+
+int test02() {
+	ut_assert( square(0) == 1 );
+	return 0;
+}
+
+int test03() {
+	ut_assert( square(1) == 1 );
+	return 0;
+}
+
 int main(int argc, char **argv) {
 	
+	printf("Starting test driver...\n\n");
+
 	// RUN TESTS HERE
+	ut_run( test01 );
+	ut_run( test02 );
+	ut_run( test03 );
+
+	printf("\nDone.\n");
+	printf("Tests run: %10d\n", tests_run);
+	printf("Tests passed: %7d\n", tests_passed);
+	printf("%3.1f%% of tests passed.\n", 100*(double)tests_passed/tests_run);
 
 	return 0;
 }
--- unittest.h
+++ unittest.h
@@ -6,9 +6,16 @@
 //
 // Simple unit test framework for C
 
-
-#define ut_assert(test) do { if(!(test)) { FAIL(); return 1; } while (0)
+#define SUCCESS() do { printf("[ OK ]\n"); \
+	tests_passed++; } while (0)
+#define FAIL() do { printf("[FAIL]\n"); \
+	tests_failed++; } while (0)
+#define ut_assert(test) do { printf("%-30s", __func__); \
+	if(!(test)) { FAIL(); } \
+	else { SUCCESS(); return 0 ; } } while (0)
 #define ut_run(test) do { int result = test(); tests_run++; \
 	if ( result ) return result; } while (0)
 
 extern int tests_run;
+extern int tests_passed;
+extern int tests_failed;
